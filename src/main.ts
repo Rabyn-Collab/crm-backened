b7640b545616918cec655c7d6ec2faef1f2622d7
Rabyn-Collab <babynshrestha64@gmail.com>
2026-07-25 19:36:06 +0545

    Clear git cache

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.getHttpAdapter().getInstance().set('trust proxy', 1); // 👈 fixed
 
   const config = new DocumentBuilder()
     .setTitle('Nest CRM API')
